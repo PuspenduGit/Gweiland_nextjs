aaa2668e89a2624bc49b05c042b62702a3be4dcc
PuspenduGit <puspendudas2002@gmail.com>
2024-08-20 19:56:46 +0530

    chore: Update Nftslideshow component with optimized options for Flickity

--- app/(components)/Nftslideshow.jsx
+++ app/(components)/Nftslideshow.jsx
@@ -11,25 +11,28 @@ const Nftslideshow = () => {
   const { items } = carousels;
   const { nfts } = items;
 
+  const flickityOptions = {
+    initialIndex: 0,
+    loop: true,
+    infinite: true,
+    draggable: false,
+    prevNextButtons: false,
+    pauseAutoPlayOnHover: false,
+    autoPlay: 1500,
+    pageDots: false,
+    wrapAround: true,
+    freeScroll: true,
+    contain: true,
+    cellAlign: "left",
+    selectedAttraction: 0.01,
+    friction: 1.2,
+    groupCells: 1,
+    adaptiveHeight: true,
+  };
+
   return (
     <div className="py-24 pointer-events-none">
-      <Flickity
-        className="carousel"
-        options={{
-          loop: true,
-          infinite: true,
-          draggable: false,
-          prevNextButtons: false,
-          pauseAutoPlayOnHover: false,
-          autoPlay: 2000,
-          pageDots: false,
-          wrapAround: true,
-          freeScroll: true,
-          contain: true,
-          cellAlign: "left",
-          groupCells: 1,
-          adaptiveHeight: true,
-        }}>
+      <Flickity className="carousel" options={{ ...flickityOptions }}>
         {nfts.map((item) => (
           <div className="carousel-cell px-8" key={item.id}>
             <Nftcard
